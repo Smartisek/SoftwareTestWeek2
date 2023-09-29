c31080d0af89dc0412f519163f91c4855745013f
Smartisek <domalipdominik@gmail.com>
2023-09-29 10:45:05 +0100

    Creating test

--- src/JunitTest.java
+++ src/JunitTest.java
@@ -5,6 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class JunitTest {
 
     @Test
-    void add2Nums() {
+    void twoPlusTwoEqualsFour() {
+        System.out.println("Test 1 - 2 + 2");
+        int num1 = 2;
+        int num2 = 2;
+        Junit j = new Junit ();
+        int expected = 4;
+        int actual = j.add2Nums(2,2);
+        assertEquals(expected,actual);
     }
 }
